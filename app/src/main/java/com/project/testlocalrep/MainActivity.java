4f4fc767bde9c9607ab85de7d85cd42b1944167b
z.sharif <z.sharif.sh@gmail.com>
2017-02-20 12:25:33 +0330

    Change Main Activity

--- app/src/main/java/com/project/testlocalrep/MainActivity.java
+++ app/src/main/java/com/project/testlocalrep/MainActivity.java
@@ -2,12 +2,16 @@ package com.project.testlocalrep;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    String value = "Barnche 1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("Name",value);
     }//end of Create
 }
