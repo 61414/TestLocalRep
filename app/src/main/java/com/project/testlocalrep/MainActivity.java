666419443667416eea951beec221e6627b90e129
z.sharif <z.sharif.sh@gmail.com>
2017-02-20 12:41:02 +0330

    Change Main Activity Value Branch 2

--- app/src/main/java/com/project/testlocalrep/MainActivity.java
+++ app/src/main/java/com/project/testlocalrep/MainActivity.java
@@ -7,11 +7,13 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     String value = "Barnche 1";
+    String Value2 = " Branch 2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Log.e("Name",value);
+        Log.e("Branch Second ", Value2);
     }//end of Create
 }
